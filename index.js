4ec2d60bece6d40af5eb7e5b672de16dfbf9d8a2
MiaMal-99 <131144828+MiaMal-99@users.noreply.github.com>
2023-04-18 16:40:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 var http = require('http');
-const port = 8000;
-
-const requestListener = function (req, res) {
-res.writeHead(200);
-    res.end(`<html><body><h1>This is HTML</h1></body></html>`);
-};
-
-const server = http.createServer(requestListener);
-server.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-});
+http.createServer(function (req, res) {
+    console.log(`Just got a request at ${req.url}!`)
+    res.write('Yo!');
+    res.end();
+}).listen(process.env.PORT || 3000);
